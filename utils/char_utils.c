4e5bbbe0905e945409935b7f691580d274aa9390
AndoGhevian <lenyvonbeer@gmail.com>
2025-03-09 21:21:57 +0400

    util add getch_many, ungetch_many

--- utils/char_utils.c
+++ utils/char_utils.c
@@ -11,3 +11,18 @@ int getch(void) {
 void ungetch(int c) {
   sch = c; 
 }
+
+#define CHBUFFSIZE 100
+char cbuf[CHBUFFSIZE];
+int cbufp = 0;
+
+int getch_many(void) {
+  return (cbufp > 0) ? cbuf[--cbufp] : getchar();
+}
+
+void ungetch_many(int c) {
+  if(cbufp >= CHBUFFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    cbuf[cbufp++] = c;
+}
--- utils/char_utils.h
+++ utils/char_utils.h
@@ -2,4 +2,6 @@
 #define HCHAR_UTILS
 void ungetch(int c);
 int getch(void);
+int getch_many(void);
+void ungetch_many(int c);
 #endif
