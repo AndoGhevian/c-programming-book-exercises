baa27a40c6e53cde23d6e93db58a154d76a292aa
AndoGhevian <lenyvonbeer@gmail.com>
2025-03-28 15:06:59 +0400

    5.11, entab

--- 5/5.11.c
+++ 5/5.11.c
@@ -2,11 +2,92 @@
 #include <stdio.h>
 #include "../utils/line_utils.h"
 
-#define MAXOUTPUT 20000
-#define MAXLEN 10000
 #define MAXPARAMS 100
 #define DEFAULTTABSTOP 4
 
+#ifndef DETABCMD
+#define MAXOUTPUT 10000 /* '\t' replaced blanks >= 1 blank */
+#define MAXLEN 10000
+main(int argc, char *argv[]) {
+  int paramlen, c, nblank, nspace;
+
+  char tabstops[MAXPARAMS];
+  char *tabstopptr = tabstops;
+
+  char input[MAXLEN];
+  char *inptr = input;
+
+  char output[MAXOUTPUT];
+  char *outptr = output;
+
+  paramlen = argc - 1;
+  if(paramlen > MAXPARAMS) {
+    printf("program parameters maximum count is %d\n", MAXPARAMS);
+    return -1;
+  }
+
+  while(tabstopptr < tabstops + paramlen)
+    *tabstopptr++ = atoi(*++argv);
+  tabstopptr = tabstops;
+
+  if(getlines(input, MAXLEN - 1) == -1) {
+    printf("input string must be not more than %d characters length\n", MAXLEN - 1);
+    return -2;
+  }
+
+  nblank = nspace = 0;
+  while(c = *inptr++)
+    switch(c) {
+      case '\n':
+        nblank = 0;
+        tabstopptr = tabstops;
+        *outptr++ = '\n';
+
+        nspace = 0;
+        break;
+      case ' ':
+        nspace++;
+        *outptr++ = ' ';
+        if(++nblank == (tabstopptr < tabstops + paramlen ? *tabstopptr : DEFAULTTABSTOP)) {
+          outptr -= nspace;
+
+          nspace = 0;
+          nblank = 0;
+          if(tabstopptr != tabstops + paramlen)
+            tabstopptr++;
+          *outptr++ = '\t';
+        }
+        break;
+      case '\t':
+        nblank = 0;
+        if(tabstopptr != tabstops + paramlen)
+          tabstopptr++;
+        *outptr++ = '\t';
+
+        nspace = 0;
+        break;
+      default:
+        if(++nblank == (tabstopptr < tabstops + paramlen ? *tabstopptr : DEFAULTTABSTOP)) {
+          nblank = 0;
+          if(tabstopptr != tabstops + paramlen)
+            tabstopptr++;
+        }
+        *outptr++ = c;
+
+        nspace = 0;
+    }
+  *outptr = '\0';
+
+
+  /* uncomment for visual representation of terminal tabstops */
+  /* printf("\n0\t0\t0\t0\t0\t\n"); */
+  printf("%s\n", output);
+
+  return 0;
+}
+#else
+#define MAXOUTPUT 20000
+#define MAXLEN 10000
 main(int argc, char *argv[]) {
   int i, paramlen, c, nblank;
 
@@ -64,3 +145,4 @@ main(int argc, char *argv[]) {
 
   return 0;
 }
+#endif
