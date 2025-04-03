4622704a99b9b7d604c5d57325ec9572ade71088
AndoGhevian <lenyvonbeer@gmail.com>
2025-04-03 13:05:55 +0400

    readlines, writelines declarations in a header

--- utils/line_utils.h
+++ utils/line_utils.h
@@ -3,5 +3,7 @@
 
 int getline_cm(char *line, int maxlen);
 int getlines(char input[], int maxinputlen);
+int readlines(char *lineptr[], int maxlines);
+void writelines(char *lineptr[], int nlines);
 
 #endif
