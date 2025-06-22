caa91d2396a0eb52b56083e8f036230ae65b3ee2
AndoGhevian <lenyvonbeer@gmail.com>
2025-06-22 12:05:29 +0400

    7.5 getch invalid implementation comment

--- 7/7.5/getch.c
+++ 7/7.5/getch.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "util.h"
 
+/* invalid implementation:
+because of statically stored pointer from outside,
+this implementation is error prone - if pointer
+is invalidated (e.g. freed) by caller, any usage
+including comparison of it will lead to undefined
+behaviour as defined by standard.
+*/
 int getch(char *src) {
   static char *srcptr = NULL;
   static char *prevreadptr = NULL;
-  if(srcptr != src)
+  if(srcptr != src) /* leads to error */
     prevreadptr = srcptr = src;
 
   if(src == NULL)
