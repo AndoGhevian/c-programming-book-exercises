75981ff42e5995de4dcf98a28993146d36b4a6ba
AndoGhevian <lenyvonbeer@gmail.com>
2025-06-25 12:50:21 +0400

    7.5 calcerror return not defined error message instead of NULL

--- 7/7.5/calcerr.c
+++ 7/7.5/calcerr.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "calcerr.h"
 
+static char const *message_notdefined = "error message not defined";
+
 static struct err_entity {
   enum calc_errors errtype;
   char *message;
@@ -9,11 +11,12 @@ static struct err_entity {
   INVALID_VAR, "invalid variable (or range).",
   INVALID_VAR_POSTFIX_OP, "invalid variable postfix operation.",
   UNKNOWN_VAR_POSTFIX_OP, "unknown variable postfix operation.",
-  INVALID_CALC_OP, "invalid operation.",
-  INVALID_VAR_IN_OP, "invalid variable (or range) in prefix operation.",
+  INVALID_CALC_OP, "invalid calculator operation.",
+  INVALID_VAR_IN_PREFIX_OP, "invalid variable (or range) in prefix operation.",
+  INVALID_VAR_IN_PRINT_OP, "invalid variable (or range) in print operation.",
   INVALID_OP_EXTRA_PART, "invalid operation excess part.",
-  UNKNOWN_CALC_OP, "unknown calc operation.",
-  NOT_IMPLEMENTED_CALC_OP, "not implemented calc operation."
+  UNKNOWN_CALC_OP, "unknown calculator operation.",
+  NOT_IMPLEMENTED_CALC_OP, "not implemented calculator operation."
 };
 
 #define NERR sizeof errmessages / sizeof(struct err_entity)
@@ -37,8 +40,7 @@ char *pop_calcerr(void) {
     if(errmessages[i].errtype == t)
       return errmessages[i].message;
 
-  /*error message is not defined for error type*/
-  return NULL;
+  return message_notdefined;
 }
 
 int has_calcerr(void) {
--- 7/7.5/calcerr.h
+++ 7/7.5/calcerr.h
@@ -7,13 +7,18 @@ enum calc_errors {
   INVALID_VAR_POSTFIX_OP,
   UNKNOWN_VAR_POSTFIX_OP,
   INVALID_CALC_OP,
-  INVALID_VAR_IN_OP,
+  INVALID_VAR_IN_PREFIX_OP,
+  INVALID_VAR_IN_PRINT_OP,
   INVALID_OP_EXTRA_PART,
   UNKNOWN_CALC_OP,
   NOT_IMPLEMENTED_CALC_OP
 };
 
+/* if there is no storage space for new errors
+they will be silently omited */
 void push_calcerr(enum calc_errors t);
+/*returns appropriate error message in stack
+or NULL if no errors left */
 char *pop_calcerr(void);
 int has_calcerr(void);
 
