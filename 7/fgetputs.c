0cf5e0efd890f2e315ab31b0915a68608db038c6
AndoGhevian <lenyvonbeer@gmail.com>
2025-07-01 16:27:31 +0400

    implement local fgets and fputs

--- 7/fgetputs.c
+++ 7/fgetputs.c
@@ -13,7 +13,27 @@ main() {
 }
 
 /* see ANSI C 4.9.7.1 */
-char *local_fgets(char *s, int n, FILE *stream);
+char *local_fgets(char *s, int n, FILE *stream) {
+  register int c;
+  register char *cs;
+
+  cs = s;
+  while(--n > 0 && (c = getc(stream)) != EOF)
+    if((*cs++ = c) == '\n')
+      break;
+  if(cs != s)
+    *cs = '\0';
+
+  if(ferror(stream))
+    return NULL;
+  return c == EOF && cs == s ? NULL : s;
+}
 
 /*see ANSI C 4.9.7.4 */
-int local_fputs(char const *s, FILE *stream);
+int local_fputs(char const *s, FILE *stream) {
+  while(*s != '\0' && putc(*s, stream) != EOF) s++;
+
+  if(ferror(stream))
+    return EOF;
+  return 0;
+}
